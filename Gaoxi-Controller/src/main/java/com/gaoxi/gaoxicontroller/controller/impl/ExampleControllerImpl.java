84691d29410358491843c29374db847d47910f45
chaibozhou <350142639@qq.com>
2017-10-27 17:13:00 +0800

    controller中加入日志

--- Gaoxi-Controller/src/main/java/com/gaoxi/gaoxicontroller/controller/impl/ExampleControllerImpl.java
+++ Gaoxi-Controller/src/main/java/com/gaoxi/gaoxicontroller/controller/impl/ExampleControllerImpl.java
@@ -2,6 +2,8 @@ package com.gaoxi.gaoxicontroller.controller.impl;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.gaoxi.gaoxicontroller.controller.ExampleController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExampleControllerImpl implements ExampleController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Reference(version = "1.0.0")
     private ExampleService exampleService;
 
     @Override
     @RequestMapping("hello")
     public String hello() {
-        return exampleService.hello();
-//        return "hello";
+        try {
+            return exampleService.hello();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("",e);
+            return e.toString();
+        }
+
     }
 }
