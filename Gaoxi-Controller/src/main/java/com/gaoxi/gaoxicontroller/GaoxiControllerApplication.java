8b4a99c4a0186c26b14d78b465f9ba73fd09b327
chaibozhou <350142639@qq.com>
2017-10-27 09:44:37 +0800

    增加Controller项目

---
+++ Gaoxi-Controller/src/main/java/com/gaoxi/gaoxicontroller/GaoxiControllerApplication.java
@@ -0,0 +1,12 @@
+package com.gaoxi.gaoxicontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaoxiControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaoxiControllerApplication.class, args);
+	}
+}
