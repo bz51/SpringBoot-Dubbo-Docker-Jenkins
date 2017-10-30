ed1640972f3f45c14c96b302dd26314932acdb31
chaibozhou <350142639@qq.com>
2017-10-30 23:50:12 +0800

    所有模块的数据库端口号改成了3306

--- Gaoxi-Controller/src/main/java/com/gaoxi/controller/example/ExampleControllerImpl.java
+++ Gaoxi-Controller/src/main/java/com/gaoxi/controller/example/ExampleControllerImpl.java
@@ -8,12 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
 @RestController
+@ResponseBody
 public class ExampleControllerImpl implements ExampleController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
