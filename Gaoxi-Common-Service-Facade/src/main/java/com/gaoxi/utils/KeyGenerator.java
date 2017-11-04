4e01781b32173ac7f63acbcfba26ac9161fc5208
chaibozhou <350142639@qq.com>
2017-11-04 19:32:08 +0800

    将主键生成方法改为公有

--- Gaoxi-Common-Service-Facade/src/main/java/com/gaoxi/utils/KeyGenerator.java
+++ Gaoxi-Common-Service-Facade/src/main/java/com/gaoxi/utils/KeyGenerator.java
@@ -13,7 +13,7 @@ public class KeyGenerator {
      * 获得一个UUID
      * @return String UUID
      */
-    private static String getKey(){
+    public static String getKey(){
         String uuid = UUID.randomUUID().toString();
         //去掉“-”符号
         return uuid.replaceAll("-", "");
