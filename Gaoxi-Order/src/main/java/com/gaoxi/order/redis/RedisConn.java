fa71c3acc1db4f94b18c75131b0d07c32acc72ee
chaibozhou <350142639@qq.com>
2017-11-14 09:37:08 +0800

    将redis转移到order系统

---
+++ Gaoxi-Order/src/main/java/com/gaoxi/order/redis/RedisConn.java
@@ -0,0 +1,49 @@
+package com.gaoxi.order.redis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 大闲人柴毛毛
+ * @date 2017/11/1 下午2:46
+ * @description redis连接信息配置类
+ */
+@Component
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisConn {
+    private String host;
+
+    private int port;
+
+    private int timeout;
+
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    @Override
+    public String toString() {
+        return "Redis [localhost=" + host + ", port=" + port + ", timeout=" + timeout + "]";
+    }
+}
---
+++ Gaoxi-Order/src/main/java/com/gaoxi/order/service/RedisServiceImpl.java
@@ -0,0 +1,128 @@
+package com.gaoxi.order.service;
+
+/**
+ * @author 大闲人柴毛毛
+ * @date 2017/11/1 下午3:15
+ * @description
+ */
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.gaoxi.facade.redis.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.io.Serializable;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+@org.springframework.stereotype.Service
+@Service(version = "1.0.0")
+public class RedisServiceImpl implements RedisService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 批量删除对应的value
+     *
+     * @param keys
+     */
+    @Override
+    public void remove(final String... keys) {
+        for (String key : keys) {
+            remove(key);
+        }
+    }
+
+    /**
+     * 批量删除key
+     *
+     * @param pattern
+     */
+    @Override
+    public void removePattern(final String pattern) {
+        Set<Serializable> keys = redisTemplate.keys(pattern);
+        if (keys.size() > 0) {
+            redisTemplate.delete(keys);
+        }
+    }
+
+    /**
+     * 删除对应的value
+     *
+     * @param key
+     */
+    @Override
+    public void remove(final String key) {
+        if (exists(key)) {
+            redisTemplate.delete(key);
+        }
+    }
+
+    /**
+     * 判断缓存中是否有对应的value
+     *
+     * @param key
+     * @return
+     */
+    @Override
+    public boolean exists(final String key) {
+        return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * 读取缓存
+     *
+     * @param key
+     * @return
+     */
+    @Override
+    public Object get(final String key) {
+        Object result = null;
+        ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
+        result = operations.get(key);
+        return result;
+    }
+
+    /**
+     * 写入缓存
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    @Override
+    public boolean set(final String key, Object value) {
+        boolean result = false;
+        try {
+            ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
+            operations.set(key, value);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 写入缓存
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    @Override
+    public boolean set(final String key, Object value, Long expireTime) {
+        boolean result = false;
+        try {
+            ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
+            operations.set(key, value);
+            redisTemplate.expire(key, expireTime, TimeUnit.SECONDS);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
