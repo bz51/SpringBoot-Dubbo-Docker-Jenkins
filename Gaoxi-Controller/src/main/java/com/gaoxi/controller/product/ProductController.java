67463132161d4a470a7e394ea309ee0917b67cbe
chaibozhou <350142639@qq.com>
2017-11-29 15:53:14 +0800

    修复了产品查询接口无法请求的问题

--- Gaoxi-Controller/src/main/java/com/gaoxi/controller/product/ProductController.java
+++ Gaoxi-Controller/src/main/java/com/gaoxi/controller/product/ProductController.java
@@ -60,7 +60,7 @@ public interface ProductController {
      * @param prodQueryReq 产品查询请求
      * @return 产品查询结果
      */
-//    @GetMapping("product")
+    @GetMapping("product")
 //    @Login
 //    @Permission("product:query")
     public Result<List<ProductEntity>> findProducts(ProdQueryReq prodQueryReq);
